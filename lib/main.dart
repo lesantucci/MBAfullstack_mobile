220e85ecaa38e6e447e936bdc7b90d31ef566916
Leandro Santucci <santucci.simoes@gmail.com>
2022-06-15 23:25:24 -0300

    adiciona mudança de nome no titulo

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'E-Commerce',
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
